f189b39f06c9bba38b8088670487affa3b00b28b
mervecakir <m.cakir88w@gmail.com>
2022-04-23 12:46:40 +0100

    doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,9 +1,12 @@
 package hello;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Hello {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("EU8 LEARNING GIT AND GITHUB");
     }
 }
